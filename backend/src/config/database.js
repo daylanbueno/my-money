4eb8b3ea4ee14230a6bd6fa463d31dc003559f5a
Dailan Bueno <daylansantos@gmail.com>
2018-11-30 21:05:45 -0200

    Customisando mensagens de erros

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -2,3 +2,7 @@ const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 module.exports = mongoose.connect('mongodb://localhost/mymoney')
 
+mongoose.Error.messages.general.required = " O atributo  '{PATH}' é Obrigatório."
+mongoose.Error.messages.Number.min = "O '{VALUE}' informado  é menor  que o limente mínimo  de '{MIN}'"
+mongoose.Error.messages.Number.min = "O '{VALUE}' informado  é maior  que o limente máximo  de '{MAX}'"
+mongoose.Error.messages.String.enum = "O '{VALUE}' não é válido para o atributo '{PATH}'. "
