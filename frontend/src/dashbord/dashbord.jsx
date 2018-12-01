8b81e38ad381405af495222a56d866cf5d458ed6
Dailan Bueno <daylansantos@gmail.com>
2018-12-01 21:37:43 -0200

    Adicionando component valuebox no dasbord, criando layout  com grid, row

--- frontend/src/dashbord/dashbord.jsx
+++ frontend/src/dashbord/dashbord.jsx
@@ -2,6 +2,8 @@ import React, {Component} from 'react'
 
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
+import ValueBox from '../common/widget/valueBox'
+import Row from '../common/layout/row'
 
 class Dashbord extends  Component {
 
@@ -10,7 +12,11 @@ class Dashbord extends  Component {
             <div>
                 <ContentHeader title='Dashbord' small='Versão 1.0'></ContentHeader>
                 <Content>
-                    Dashbord
+                    <Row>
+                        <ValueBox cols='12 4'  color='green'  icon='bank' value='R$ 10,00' text='Total Créditos'/>
+                        <ValueBox cols='12 4'  color='red' icon='bank' value='R$ 10,00' text='Total Debitos'/>
+                        <ValueBox cols='12 4'  color='blue' icon='money' value='R$ 0,00' text='Valor consolidado'/>
+                    </Row>
                 </Content>
             </div>
         )
