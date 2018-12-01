73a1d9e87e51eb6da3f47887c2ce7f90a8e806f9
Dailan Bueno <daylansantos@gmail.com>
2018-12-01 21:46:49 -0200

    Configurando redux, criando store e reducer

---
+++ frontend/src/main/reduces.js
@@ -0,0 +1,9 @@
+import {combineReducers } from 'redux'
+
+const rootReducers  = combineReducers({
+    dashboard: () => ({
+        summary:{ credit:100,  debt: 50}
+    })
+})
+
+export default rootReducers
