dbca944de27cf227533c07d3df694f5520cb7a1d
Dailan Bueno <daylansantos@gmail.com>
2018-12-01 18:42:14 -0200

    Finalizando template com os menus

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+    <footer className='main-footer'> 
+        <strong> 
+            Desenvolvido&copy; 2018
+            <a  target='_blank'> Dailan Bueno dos Santos</a>.
+        </strong>
+    </footer>
+)
--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import '../common/template/dependencias'
 import Header from '../common/template/header'
+import SideBar from '../common/template/sideBar'
+import Footer from '../common/template/footer'
 
 export default props => (
     <div className='wrapper'>
         <Header/>
+        <SideBar/>
+        <div className='content-wrapper'>
+            <h3>Conteúdo</h3>    
+         </div>
+        <Footer/>
     </div>
 )
