984acecb703188a1fe90407870fec1d5aad6d0a3
Dailan Bueno <daylansantos@gmail.com>
2018-11-30 22:45:26 -0200

    configurando cors, errorHander, consutla paginada etc.

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -3,9 +3,14 @@ const port = 3003
 const bodyParser = require('body-parser')
 const  express = require('express')
 const server = express()
+const allowCors = require('./cors')
+const queryParse = require('express-query-int')
 
 server.use(bodyParser.urlencoded({ extended: true}))
 server.use(bodyParser.json())
+server.use(allowCors)
+server.use(queryParse())
+
 
 server.listen(port, function(){
     console.log(`BACKEND ESTÁ RODANDO NA PROTA ${port}.`)
