25b3ae9839e06642d298de1b3dbcc2d76e50c303
Dailan Bueno <daylansantos@gmail.com>
2018-12-02 22:59:31 -0200

    Ajuste de Component tabs para controle de render.

--- frontend/src/common/tab/tabContent.jsx
+++ frontend/src/common/tab/tabContent.jsx
@@ -1,14 +1,18 @@
 import React, { Component } from 'react'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
+import If  from '../operator/if'
 
 class TabContent extends  Component {
     render() {
         const selected = this.props.tab.selected === this.props.id
+        const visible = this.props.tab.visible[this.props.id]
         return (
-            <div className={`tab-pane ${ selected ? 'active': ''}`} id={this.props.id}>
-                {this.props.children}
-            </div>
+            <If test={visible}>
+                <div className={`tab-pane ${ selected ? 'active': ''}`} id={this.props.id}>
+                    {this.props.children}
+                </div>
+            </If>
         )
     }
 }
--- frontend/src/common/tab/tabHeader.jsx
+++ frontend/src/common/tab/tabHeader.jsx
@@ -3,11 +3,14 @@ import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 
 import { selectTab }  from './tabActions'
+import If  from '../operator/if'
 
 class TabHeader  extends Component {
     render() {
         const selected = this.props.tab.selected === this.props.target
+        const visible  = this.props.tab.visible[this.props.target]
         return (
+            <If  test={visible}>
              <li className={selected ? 'active': ''}>
                  <a href='javascript:;' 
                     onClick={() => this.props.selectTab(this.props.target)}
@@ -15,6 +18,7 @@ class TabHeader  extends Component {
                     <i className={`fa fa-${this.props.icon}`}> </i> {this.props.label}
                 </a>
              </li>
+             </If>
         )
     }
 }
