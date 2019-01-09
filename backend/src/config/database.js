b2006e5be421c116561381dc7d5c349afaf698dd
Dailan Bueno <daylansantos@gmail.com>
2019-01-09 20:49:21 -0200

    ajuste aplicação

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
-module.exports = mongoose.connect('mongodb://localhost/mymoney')
+
+const url = process.env.MONGOLAB_URI ? process.env.MONGOLAB_URI : 'mongodb://localhost/mymoney'
+
+module.exports = mongoose.connect(url)
 
 mongoose.Error.messages.general.required = " O atributo  '{PATH}' é Obrigatório."
 mongoose.Error.messages.Number.min = "O '{VALUE}' informado  é menor  que o limente mínimo  de '{MIN}'"
