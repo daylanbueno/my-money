dade75b9326bc79107a174b47a7ebbda3bf189ad
Dailan Bueno <daylansantos@gmail.com>
2019-01-08 23:10:40 -0200

    Tirando obrigatóriedade para debito e créditos

--- backend/src/api/billingCycle/billingCycle.js
+++ backend/src/api/billingCycle/billingCycle.js
@@ -2,23 +2,22 @@ const restfull = require('node-restful')
 const mongoose = restfull.mongoose
 
 const creditSchema = new mongoose.Schema({
-    name: { type: String , required: true },
-    value: { type: Number,  min:0, required: true }
+    name: { type: String , required: false },
+    value: { type: Number,  min:0, required: false }
 }) 
 
-
 const debtSchema = new mongoose.Schema({
-    name: { type: String , required: true },
-    value: { type: Number,  min:0, required: [true, 'Informe o valor do débito'] },
-    status: { type:String, required: false, uppercase: true, enum:['PAGO','PENDENTE','AGENDADO'] }
+    name: { type: String , required: false },
+    value: { type: Number,  min:0, required: [false, 'Informe o valor do débito'] },
+    status: { type:String, required: false, uppercase: false, enum:['PAGO','PENDENTE','AGENDADO'] }
 }) 
 
 const billingCycleSchema = new mongoose.Schema({
     name: { type: String , required: true },
-    month: { type:Number,  min:1 , max:12,  required:true },
-    year: { type:Number, min:1970, max:2100, required:true },
+    month: { type:Number,  min:1 , max:12,  required:[true, 'O Mês é obrigatório'] },
+    year: { type:Number, min:1970, max:2100, required:[true, 'O Ano é obrigatório'] },
     credits: [creditSchema],
-    debts: [debtSchema]
+    debts: [debtSchema], 
 })
 
 module.exports = restfull.model('BillingCycle',billingCycleSchema)
